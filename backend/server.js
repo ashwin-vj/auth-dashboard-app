8d18bf31b84226cf445687d4685cda5cd524b6e1
Ashwin Vijay <ashwinvijay108.com>
2026-03-25 10:57:50 +0300

    port change

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
